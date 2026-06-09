fae86764294c1b6d1a4bb477292b63d763dc06fd
Loreana <39282726+Loreana-Castaldo@users.noreply.github.com>
2026-06-09 00:07:07 +0000

    ejercicio 9, kruskal

--- src/main/java/ucu/edu/aed/tda/grafo/model/Implementaciones/UndirectedGraphAlgorithm.java
+++ src/main/java/ucu/edu/aed/tda/grafo/model/Implementaciones/UndirectedGraphAlgorithm.java
@@ -1,6 +1,12 @@
 package ucu.edu.aed.tda.grafo.model.Implementaciones;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import ucu.edu.aed.tda.grafo.IUndirectedGraph;
@@ -12,8 +18,38 @@ public class UndirectedGraphAlgorithm implements IUndirectedGraphAlgorithm {
 
     @Override
     public <V, D extends WeightedEdge> IUndirectedGraph<V, D> kruskal(IUndirectedGraph<V, D> graph) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'kruskal'");
+       
+       //arbol de expansion minima
+       //List<Edge<V, D>> mst = new LinkedList<>();
+
+        //obtiene todas las aristas del grafo y las ordena por peso
+        List<Edge<V, D>> aristas = new ArrayList<>(graph.aristas());
+        aristas.sort(Comparator.comparingDouble(e -> e.dato().getWeight()));
+
+        Set<Edge<V, D>> mstEdges = new LinkedHashSet<>();
+        Set<V> mstVertices = new LinkedHashSet<>(graph.vertices());
+
+        for (Edge<V, D> edge : aristas) {
+
+            //construir grafo temporal
+            Set<Edge<V, D>> tempEdges = new LinkedHashSet<>(mstEdges);
+            tempEdges.add(edge);
+
+            Graph<V, D> tempGraph = new UndirectedGraph<>(mstVertices, tempEdges, null);
+            
+            //si no genera ciclo se acepta la arista
+             if (!tempGraph.tieneCiclos()    ) {
+                mstEdges.add(edge);
+            }
+
+            //optimizacion basica, mst compelto
+            if (mstEdges.size() == mstVertices.size() - 1) {
+                break;
+            }   
+        }
+        return new UndirectedGraph<>(mstVertices, mstEdges, null);  
+
+
     }
 
     @Override
